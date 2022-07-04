0102206e47cc570687096883d7412dff6239a3e6
Marcelo <gastonfoche@gmail.com>
2022-07-04 19:55:04 -0300

    comentarios

--- 1-EntradaSalida/jsEntradaSalida-02.js
+++ 1-EntradaSalida/jsEntradaSalida-02.js
@@ -3,7 +3,7 @@ Debemos lograr tomar un nombre con 'prompt'
 y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
-	let nombre = prompt("Ingrese su nombre:");
-	alert(nombre);
+	let nombre = prompt("Ingrese su nombre:");// Declaro la variable con let nombre y usando el operador de asignación le asigno un valor.
+	alert(nombre);// Muestro el valor de la variable.
 }
 
