56cdf2505936dfaa10122a50c3967717d1cb53f1
Marcelo <gastonfoche@gmail.com>
2022-07-04 20:14:34 -0300

    Editando comentarios

--- 1-EntradaSalida/jsEntradaSalida-01.js
+++ 1-EntradaSalida/jsEntradaSalida-01.js
@@ -1,8 +1,10 @@
 /*
+Alumno: Marcelo Gastón Pérez Foche.
+E/S - Ejercicio 01.
 Al presionar el  botón, 
 se debe mostrar un mensaje como el siguiente "Esto funciona de maravilla"*/
 function mostrar()
 {
-	alert("Esto funciona de maravilla");
+	alert("Esto funciona de maravilla");// muestro por el recuadro de la función alert el mensaje "Esto funciona de maravilla".
 }
 
--- 1-EntradaSalida/jsEntradaSalida-02.js
+++ 1-EntradaSalida/jsEntradaSalida-02.js
@@ -1,9 +1,11 @@
 /*
+Alumno: Marcelo Gastón Pérez Foche.
+E/S - Ejercicio 02.
 Debemos lograr tomar un nombre con 'prompt' 
 y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
-	let nombre = prompt("Ingrese su nombre:");// Declaro la variable con let nombre y usando el operador de asignación le asigno un valor.
-	alert(nombre);// Muestro el valor de la variable.
+	let nombre = prompt("Ingrese su nombre:");// Declaro la variable nombre con let y usando el operador de asignación le asigno el valor obtenido por la función prompt.
+	alert(nombre);// Muestro el valor asignado a la variable por el recuadro de la función alert.
 }
 
