de788f1487c9c06e03cfdfbbfd115e23134ed45d
Marcelo <gastonfoche@gmail.com>
2022-07-05 21:12:06 -0300

    ej 8 y 9

--- 1-EntradaSalida/jsEntradaSalida-08.js
+++ 1-EntradaSalida/jsEntradaSalida-08.js
@@ -1,9 +1,49 @@
 /*
+Alumno: Marcelo Gastón Pérez Foche.
+E/S - Ejercicio 08.
 Debemos lograr tomar Los numeros por ID ,
 transformarlos a enteros (parseInt),realizar la operación correcta y 
 mostrar el resto entre el dividendo y el divisor.
 ej.: "El resto es 0 ."*/
 function SacarResto()
 {
-	alert("ok");
+	let numeroUno;// declaro la variable numeroUno
+
+	numeroUno = document.getElementById("txtIdNumeroDividendo").value;
+	/*obtengo el valor obtenido con document.getElementById("").value
+		y se lo asigno a la variable numeroUno.
+	*/ 
+
+	numeroUno = parseInt(numeroUno);
+	/*
+		paso a número el valor de la variable con la función parseInt
+		y se lo asigno a la variable numeroUno.
+	*/ 
+
+
+	let numeroDos;// declaro la variable numeroDos
+	
+	numeroDos = document.getElementById("txtIdNumeroDivisor").value;
+	/*obtengo el valor obtenido con document.getElementById("").value
+		y se lo asigno a la variable numeroDos.
+	*/ 
+
+	numeroDos = parseInt(numeroDos);
+	/*paso a número el valor de la variable con la función parseInt
+		y se lo asigno a la variable numeroDos.
+	*/ 
+		
+
+	let resultado = numeroUno % numeroDos;
+	/*declaro la variable y le asigno el valor del resultado  
+		del resto de la división entre el valor de las variables
+	 	numeroUno y numeroDos */ 
+
+
+	alert("El resultado del resto es" + " " + resultado);
+	/*muestro el resultado con el literal "El resultado del resto es"
+	 concatenado con un espacio en blanco " " y el valor asignado
+	  a la variable resultado usando la función alert*/
 }
+
+//https://onlinegdb.com/p19wVONKx
--- 1-EntradaSalida/jsEntradaSalida-09.js
+++ 1-EntradaSalida/jsEntradaSalida-09.js
@@ -1,10 +1,47 @@
 /*
+Alumno: Marcelo Gastón Pérez Foche.
+E/S - Ejercicio 09.
 Debemos lograr tomar el importe por ID ,
 transformarlo a entero (parseInt), luego
 mostrar el importe con un aumento del 10 %
 en el cuadro de texto "RESULTADO".*/
 function mostrarAumento()
 {
+	let sueldo;// declaro la variable sueldo.
 
-	alert("ok");
+	sueldo = document.getElementById("txtIdSueldo").value;
+	/*
+		obtengo el valor obtenido con document.getElementById("").value
+		y se lo asigno a la variable sueldo.
+	*/ 
+
+	sueldo = parseInt(sueldo);
+	/*
+		paso a número el valor de la variable con la función parseInt
+		y se lo asigno a la variable sueldo.
+	*/ 
+
+
+	let RESULTADO;// declaro la variable RESULTADO
+	
+	RESULTADO = sueldo * 0.10;
+	/*
+		tomo el valor del sueldo que ingresó el usuario y lo multiplico por 0.10 para obtener el 
+		10% del sueldo ingresado por el usuario finalmente asigno ese valor a la variable RESULTADO. 
+	*/ 
+
+
+	let resultadoMasPorcentaje;// declaro la variable resultadoMasPorcentaje. 
+
+	resultadoMasPorcentaje = RESULTADO + sueldo;
+	/*
+		tomo el valor del 10 porciento del sueldo, lo sumo al sueldo original y
+		asigno el valor obtenido a la variable resultadoMasPorcentaje. 
+	*/ 
+
+
+	document.getElementById("txtIdResultado").value = resultadoMasPorcentaje;
+	/*muestro el valor asignado a la variable resultadoMasPorcentaje en el cuadro de texto txtIdResultado*/
 }
+
+//https://onlinegdb.com/zYLPmhL_W
