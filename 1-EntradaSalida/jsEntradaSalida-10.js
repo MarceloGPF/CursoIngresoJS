85656b5f64836ad82e222ad1914f22ae8228789f
Marcelo <gastonfoche@gmail.com>
2022-07-07 18:15:09 -0300

    meh

--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -1,9 +1,11 @@
 /*
+Alumno: Marcelo Gastón Pérez Foche.
+E/S - Ejercicio 10.
 Debemos lograr tomar el importe por ID.
 Transformarlo a entero (parseInt), luego
 mostrar el importe con un Descuento del 25 %
 en el cuadro de texto "RESULTADO"*/
 function mostrarAumento()
 {
-	alert("ok");
+	
 }
