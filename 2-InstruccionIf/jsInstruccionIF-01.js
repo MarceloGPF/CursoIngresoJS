6d58bac477174a29b4ef9de293f42da0f7c4973c
Marcelo <gastonfoche@gmail.com>
2022-07-09 10:19:11 -0300

    InstrucciónIF-01.

--- 2-InstruccionIf/jsInstruccionIF-01.js
+++ 2-InstruccionIf/jsInstruccionIF-01.js
@@ -1,6 +1,28 @@
+/*
+Alumno: Marcelo Gastón Pérez Foche.
+InstrucciónIF-01.
+*/
+
 function mostrar()
 {
 	//tomo la edad  
-	alert("ok");
+	let edad;
+
+	edad = document.getElementById("txtIdEdad").value;
+	edad = parseInt(edad);
+	
+	if (edad == 15){
+		
+		alert("niña bonita");
+
+	}else{
+
+		alert("no es niña bonita");
+
+	};
+
+
+
+
 
 }//FIN DE LA FUNCIÓN
