9db58cc165b355c9a6de81fabe3ce9f0b8d14f2d
Marcelo <gastonfoche@gmail.com>
2022-07-14 17:29:28 -0300

    instancia final TP Ferrete Iluminación.

--- 8-TPs/04-jsFerreteIluminacion.js
+++ 8-TPs/04-jsFerreteIluminacion.js
@@ -26,6 +26,10 @@ function CalcularPrecio ()
     let resultado;
     let mensaje;
     let aPagar;
+    const IMPUESTO = 0.10;
+    let porcentajeImpuestoSobreTotalConDescuento;
+    let totalConImpuesto; 
+    let mensajeImpuesto;
    
     
 
@@ -43,52 +47,98 @@ function CalcularPrecio ()
     
     
         
-    if(cantidadDeLamparas >= 6 && marca != "ArgentinaLuz"){                   // A
+    if(cantidadDeLamparas >= 6 && cantidadDeLamparas != 5){                   // A
 
         descuento = 0.50;
         
     }else{
 
-        if(cantidadDeLamparas > 5 && marca == "ArgentinaLuz"){          //B
+        if(cantidadDeLamparas == 5 && marca == "ArgentinaLuz"){          //B
     
             descuento = 0.40;
                
-        }
+            
+        }else{
+
+            if(marca != "ArgentinaLuz" && cantidadDeLamparas >= 4 && cantidadDeLamparas != 3){          
+
+                descuento = 0.30;
+
+            }else{ 
+
+                if(cantidadDeLamparas == 4 && marca == "ArgentinaLuz" || marca == "FelipeLamparas" && cantidadDeLamparas != 3){           //C
+
+                        descuento = 0.25;
+
+                }else{
+
+                        if(marca != "ArgentinaLuz" && marca != "FelipeLamparas" && cantidadDeLamparas == 4){
+
+                            descuento = 0.20;
+
+                        }else{
+
+                            if(cantidadDeLamparas == 3 && marca == "ArgentinaLuz"){                   //D
+                                                                                                                                             
+                                    descuento = 0.15;
+                            }else{
+
+                                if(marca == "FelipeLamparas"){
+
+                                    descuento = 0.10;
+
+                                }else{
+
+                                    if(marca != "ArgentinaLuz" && marca != "FelipeLamparas"){
+
+                                        descuento = 0.10;
+
+                                    }
+
+                                }
+
+                            }
+
+                        }
+
+                }
+
+            }
+        }        
+        
+    }
+
+    
+
+    
+    
+    resultado = calculoTotal * descuento;
+    
+    aPagar =  calculoTotal - resultado;
+
+
+    if(aPagar >= 120){
+
+        porcentajeImpuestoSobreTotalConDescuento = aPagar * IMPUESTO; 
+
+        totalConImpuesto =  aPagar + porcentajeImpuestoSobreTotalConDescuento;
+
+        mensajeImpuesto = "Usted pago " + ( porcentajeImpuestoSobreTotalConDescuento + 100 ) +  " de IIBB"
+
     }
     
+    mensaje = "Usted tiene un descuento de " + (descuento * 100) + " %";
+    
+    alert(mensaje);
+    
+    document.getElementById("txtIdprecioDescuento").value = aPagar;
     
     
     
+    //E.	Si el importe final con descuento suma más de $120  se debe sumar un 10% de ingresos brutos en informar del impuesto con el siguiente mensaje:
+    //       ”Usted pago X de IIBB.”, siendo X el impuesto que se pagó.    
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        
-        resultado = calculoTotal * descuento;
-        
-        aPagar =  calculoTotal - resultado;
-        
-        mensaje = "Usted tiene un descuento de " + (descuento * 100) + " %";
-
-        alert(mensaje);
-        
-        document.getElementById("txtIdprecioDescuento").value = aPagar;
-        
-        
-        
-    }
-    
     
     
     
